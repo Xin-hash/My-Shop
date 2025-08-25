662c40a871f15ba23e7135bfcd52d93963cb7c68
Rodel Cruz <rodel.cruz0012@gmail.com>
2025-08-25 23:32:13 +0800

    fix

--- frontend/src/api/products.js
+++ frontend/src/api/products.js
@@ -1,7 +1,7 @@
 // src/api/products.js
 import axios from "axios";
 
-const API_URL = import.meta.env.REACT_APP_API_URL || "http://localhost:5000/api/products"; // adjust if backend runs elsewhere
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/products"; // adjust if backend runs elsewhere
 
 export const getProducts = async () => {
   return await axios.get(API_URL);
